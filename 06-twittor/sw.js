da6e0cb8e90de286ae6e1fc8a299b7058f1c3a1e
SupineDread <hazielfe@gmail.com>
2020-01-08 22:20:26 -0600

    After all this time

--- 06-twittor/sw.js
+++ 06-twittor/sw.js
@@ -1,3 +1,5 @@
+importScripts('js/sw-utils.js');
+
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
@@ -39,3 +41,13 @@ self.addEventListener('activate', e => {
     });
     e.waitUntil(respuesta);
 });
+
+self.addEventListener('fetch', e => {
+    const respuesta = caches.match(e.request).then(res => {
+        if(res) return res;
+        return fetch(e.request).then(newResponse => {
+            return updateDynamicCache(DYNAMIC_CACHE, e.request, newResponse);
+        });
+    });
+    e.respondWith(respuesta)
+})
